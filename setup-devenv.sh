19a7f0de6ba9c341376f07603a5d62dc31acdbc2
Yohan de Rose <yohanderose@gmail.com>
2021-04-26 12:46:45 +1200

    feat: Adding commitzen alias to gitconfig and setting up condarc to install autopep and pylint by default when conda creating new envs

--- setup-devenv.sh
+++ setup-devenv.sh
@@ -44,6 +44,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O mi
 chmod +x miniconda.sh
 bash miniconda.sh -b -p $HOME/.miniconda3
 rm miniconda.sh
+rm ~/.condarc
+ln ~/.dotfiles/condarc ~/.condarc
 
 # Install git workflow 
 pip install -U Commitizen autopep8 pylint
